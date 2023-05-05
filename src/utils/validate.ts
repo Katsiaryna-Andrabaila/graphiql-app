1b6b0f1120e7a2d425e4cccbc80816e814c7c652
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-05-05 12:46:42 +0300

    fix: fix regular expression

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,5 +2,7 @@ export const validateEmail = (email: string) => (email ? /.+@.+\..+/i.test(email
 
 export const validatePassword = (password: string) =>
   password
-    ? /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[0-9a-zA-Z!@#$%^&*]{8,}/.test(password)
+    ? /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!"#$%&'()*+,-./\\:;<=>?@[\]^_`{|}~])[0-9a-zA-Z!"#$%&'()*+,-./\\:;<=>?@[\]^_`{|}~]{8,}/.test(
+        password
+      )
     : false;
