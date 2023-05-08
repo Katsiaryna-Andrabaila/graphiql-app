d40414fc576b127afe558a2a40d5aba63c5e3961
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-05-08 14:31:35 +0200

    fix: delete console.log

--- src/HOC/Private.tsx
+++ src/HOC/Private.tsx
@@ -2,7 +2,6 @@ import { Navigate } from 'react-router-dom';
 import { RequireAuthProps } from '../types/types';
 
 export const RequireAuth = ({ children, redirectPath = "/about", redirect }: RequireAuthProps) => {
-  console.log(redirect)
   if (redirect) {
     return <Navigate to={redirectPath} replace />;
   }
