61e67b20931635d148fb359a85b074f8fd11cc02
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-05-28 14:54:45 +0300

    refactor: remove unnecessary code

--- src/HOC/Provider.tsx
+++ src/HOC/Provider.tsx
@@ -2,8 +2,7 @@ import { createContext, useState, ReactNode, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TypeAppContext } from '../types/types';
 import { useLocalStorage } from '@mantine/hooks';
-import { getAuth } from 'firebase/auth';
-import { app, logOut } from '../utils/firebase';
+import { auth, logOut } from '../utils/firebase';
 import { DEFAULT_VALUES } from '../constants/constants';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
@@ -11,7 +10,6 @@ const initialContext = { isAuth: false, isLogin: false, lang: 'en', history: [] 
 export const AppContext = createContext<TypeAppContext>(initialContext);
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  const auth = getAuth(app);
   const [user] = useAuthState(auth);
 
   const [isAuth, setIsAuth] = useState(!!user);
